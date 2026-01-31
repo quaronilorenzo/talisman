695862a61af4e28c07e1aa62cb5337cd6848dce7
Quaro <135996757+quaronilorenzo@users.noreply.github.com>
2026-01-31 11:16:39 +0100

    Added raw player

---
+++ src/PlayerCostants.java
@@ -0,0 +1,5 @@
+public class PlayerCostants {
+    public static final int STARTING_HEALTH = 100;
+    public static final int BAREKNUCKLE_DAMAGE = 100;
+    public static final int STARTING_DEFENSE = 0;
+}
